8308bfbbcd1ca7529e8d7466c3e94a072628f809
ChenYenDu <chun.yen.tu@gmail.com>
2021-05-13 00:05:26 +0800

    add email verification

--- src/pages/store/AuthModule.js
+++ src/pages/store/AuthModule.js
@@ -1,29 +1,29 @@
-import firebase from 'firebase/app';
+import firebase from "firebase/app";
 
 const AuthModule = {
   state: {
     signed_up: false,
   },
-  getters:{
-    signed_up: state => state.signed_up
+  getters: {
+    signed_up: (state) => state.signed_up,
   },
   mutations: {
-    setSignedUp(state, payload){
-      state.signed_up = payload
-    }
+    setSignedUp(state, payload) {
+      state.signed_up = payload;
+    },
   },
   actions: {
-    signUp({ commit }, payload) {
+    signUp({ commit, dispatch }, payload) {
       firebase
         .auth()
         .createUserWithEmailAndPassword(payload.email, payload.password)
         .then((data) => {
-          firebase.database().ref('users').child(data.user.uid).set({
+          firebase.database().ref("users").child(data.user.uid).set({
             uid: data.user.uid,
             name: payload.name,
             email: payload.email,
             emailverified: false,
-            photo_url: payload.photoURL
+            photo_url: payload.photoURL,
           });
           let newuser = data.user;
           newuser
@@ -32,17 +32,31 @@ const AuthModule = {
               photoURL: payload.photoURL,
             })
             .then(() => {
-              commit('setSignedUp', true)
-              console.log('updated profile');
+              dispatch("sentVerification")
+              commit("setSignedUp", true);
+              console.log("updated profile");
             })
             .catch((err) => {
               console.log(err.message);
-              commit('setAlertMessage', err.message)
+              commit("setAlertMessage", err.message);
             });
         })
         .catch((err) => {
           console.log(err.message);
-          commit('setAlertMessage', err.message)
+          commit("setAlertMessage", err.message);
+        });
+    },
+    sentVerification({ commit }) {
+      var user = firebase.auth().currentUser;
+
+      user
+        .sendEmailVerification()
+        .then(function () {
+          // Email sent.
+          commit('setAlertMessage', `A verification email has been sent to ${user.email}`)
+        })
+        .catch(function (error) {
+          // An error happened.
         });
     },
   },
