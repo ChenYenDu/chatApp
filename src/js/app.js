8b7ffb09cf3c5cf12240053f2436e6f68d8a9596
ChenYenDu <chun.yen.tu@gmail.com>
2021-05-17 23:59:37 +0800

    Let app remember the login state

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,56 @@
 // Import Vue
-import { createApp } from 'vue';
+import { createApp, registerRuntimeCompiler } from "vue";
 
 // Import Framework7
-import Framework7 from 'framework7/lite-bundle';
+import Framework7 from "framework7/lite-bundle";
 
 // Import Framework7-Vue Plugin
-import Framework7Vue, { registerComponents } from 'framework7-vue/bundle';
+import Framework7Vue, { registerComponents } from "framework7-vue/bundle";
 
 // Import Framework7 Styles
-import 'framework7/framework7-bundle.css';
+import "framework7/framework7-bundle.css";
 
 // Import Icons and App Custom Styles
-import '../css/icons.css';
-import '../css/app.css';
+import "../css/icons.css";
+import "../css/app.css";
 
 // Import App Component
-import App from '../components/app.vue';
-import store from '../pages/store/store';
+import App from "../components/app.vue";
+import store from "../pages/store/store";
+import firebase from "firebase";
 
 // Init Framework7-Vue Plugin
 Framework7.use(Framework7Vue);
 
-// Init App
-const app = createApp(App);
-app.use(store)
+let newapp = null;
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    store.commit("setSignedIn", true);
+  } else {
+    // No user is signed in.
+    store.commit("setSignedIn", false);
+  }
 
-// Register Framework7 Vue components
-registerComponents(app);
+  if (!newapp) {
+    // Init App
+    newapp = createApp(App);
+    newapp.use(store);
 
-// Mount the app
-app.mount('#app');
+    // Register Framework7 Vue components
+    registerComponents(newapp);
+
+    // Mount the app
+    newapp.mount("#app");
+  }
+});
+
+// // Init App
+// const app = createApp(App);
+// app.use(store);
+
+// // Register Framework7 Vue components
+// registerComponents(app);
+
+// // Mount the app
+// app.mount("#app");
